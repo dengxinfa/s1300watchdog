ff1cde523106054042e398a0c3c8a6708f79ee37
dengxinfa <1712866140@qq.com>
2018-07-12 18:14:05 +0800

    添加喂狗程序

---
+++ watchdog.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+[ -n "$(grep "DK01" /proc/device-tree/model 2>/dev/null)" ] && model="b1300"
+[ -n "$(grep "DK04" /proc/device-tree/model 2>/dev/null)" ] && model="s1300"
+
+case "$model" in
+"b1300")
+	GPIO=58
+	;;
+"s1300")
+	GPIO=39
+	;;
+esac
+
+echo $GPIO >/sys/class/gpio/export
+
+echo out >/sys/class/gpio/gpio${GPIO}/direction
+
+feed_dog()
+{
+ 	local count=5
+        for i in $(seq 1 $count);
+        do
+                echo 1 >/sys/class/gpio/gpio${GPIO}/value
+                sleep 0.05
+                echo 0 >/sys/class/gpio/gpio${GPIO}/value
+                sleep 0.05
+        done
+}
+
+detect_reboot_by_hw_watchdog()
+{	
+	echo in >/sys/class/gpio/gpio$GPIO/direction
+	for i in $(seq 1 4)
+	do
+		v0=$(cat /sys/class/gpio/gpio${GPIO}/value)
+		sleep 0.1
+		v1=$(cat /sys/class/gpio/gpio${GPIO}/value)
+		sleep 0.1
+		v2=$(cat /sys/class/gpio/gpio${GPIO}/value)
+		sleep 0.1
+		v3=$(cat /sys/class/gpio/gpio${GPIO}/value)
+		sleep 0.1
+		if [ "$v0" = "$v1" -a "$v1" = "$v2" -a "$v2" = "$v3" ]; then
+			no_pulse=$(($no_pulse + 1));
+		fi
+		sleep 0.4
+	done
+}
+
+feed_dog
+no_pulse=0
+
+detect_reboot_by_hw_watchdog
+
+[ "$no_pulse" -lt 4 ] && echo "$(date +%Y-%m-%d/%T) reset" >> /reset_by_hw_watchdog
+
+while true; do
+	feed_dog
+	sleep 3
+done
+
+
